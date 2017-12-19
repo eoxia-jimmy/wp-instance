d6a6312ec45a6678e59a5dfa86c58c134fca62bc
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-12-19 17:34:02 +0100

    0.1.0: Add routes

--- core/action/wp-instance-core.action.php
+++ core/action/wp-instance-core.action.php
@@ -26,7 +26,58 @@ class WP_Instance_Core_Action {
 	 * @since 0.1.0
 	 * @version 0.1.0
 	 */
-	public function __construct() {}
+	public function __construct() {
+		add_action( 'rest_api_init', array( $this, 'callback_api_init' ), 99 );
+	}
+
+	/**
+	 * Add routes for add/put/get instances of WordPress.
+	 *
+	 * @since 0.1.0
+	 * @version 0.1.0
+	 *
+	 * @return void
+	 */
+	public function callback_api_init() {
+		register_rest_route( 'wp-instance/v1', '/instance/(?P<id>\d+)', array(
+			array(
+				'methods'             => \WP_REST_Server::CREATABLE,
+				'callback'            => array( WP_Instance_Core_Class::g(), 'create' ),
+				'permission_callback' => function() {
+					if ( get_current_user_id() ) {
+						return true;
+					}
+
+					return false;
+				},
+			),
+			array(
+				'methods'             => \WP_REST_Server::READABLE,
+				'callback'            => array( WP_Instance_Core_Class::g(), 'get' ),
+				'permission_callback' => function() {
+					if ( get_current_user_id() ) {
+						return true;
+					}
+
+					return false;
+				},
+			),
+		) );
+
+		register_rest_route( 'wp-instance/v1', '/instance/(?P<id>\d+)', array(
+			array(
+				'methods'             => \WP_REST_Server::DELETABLE,
+				'callback'            => array( WP_Instance_Core_Class::g(), 'delete' ),
+				'permission_callback' => function() {
+					if ( get_current_user_id() ) {
+						return true;
+					}
+
+					return false;
+				},
+			),
+		) );
+	}
 }
 
 new WP_Instance_Core_Action();
--- core/class/wp-instance-core.class.php
+++ core/class/wp-instance-core.class.php
@@ -29,17 +29,105 @@ class WP_Instance_Core_Class extends \eoxia\Singleton_Util {
 	protected function construct() {}
 
 	/**
-	 * Affiches la vue pour mêttre à jour les données de DigiRisk dans le réseau.
+	 * Créer une instance de WordPress pour l'utilisateur current_user_id.
 	 *
-	 * @since 0.2.0
-	 * @version 0.2.0
+	 * @since 0.1.0
+	 * @version 0.1.0
 	 *
-	 * @return void
+	 * @return array All WordPress instances for this user founded in user meta "\eoxia\Config_Util::$init['wp_instance']->meta_wp_instance".
 	 */
-	public function display() {
-		$done = ( ! empty( $_GET['done'] ) && 'true' == $_GET['done'] ) ? true : false;
+	public function create( $request ) {
+		if ( empty( $request['id'] ) || empty( $request['app_id'] ) || empty( $request['app_title'] ) || empty( $request['app_url'] ) || empty( $request['app_username'] ) || empty( $request['app_password'] ) ) {
+			return false;
+		}
 
-		require_once PLUGIN_DIGIRISK_DASHBOARD_PATH . '/core/view/upgrade.view.php';
+		$user_id = (int) $request['id'];
+
+		$app = array(
+			'id'       => (int) $request['app_id'],
+			'title'    => sanitize_text_field( $request['app_title'] ),
+			'url'      => sanitize_url( $request['app_url'] ),
+			'username' => sanitize_text_field( $request['app_username'] ),
+			'password' => sanitize_text_field( $request['app_password'] ),
+		);
+
+		$instances = get_user_meta( $user_id, \eoxia\Config_Util::$init['wp_instance']->meta_wp_instance, true );
+
+		if ( empty( $instances ) ) {
+			$instances = array();
+		}
+
+		$instances[ (int) $app['id'] ] = $app;
+
+		update_user_meta( $user_id, \eoxia\Config_Util::$init['wp_instance']->meta_wp_instance, $instances );
+
+		return $instances;
+	}
+
+	/**
+	 * Récupères les instances de WordPress pour l'utilisateur current_user_id.
+	 *
+	 * @since 0.1.0
+	 * @version 0.1.0
+	 *
+	 * @return array Founded WordPress instances in user meta "\eoxia\Config_Util::$init['wp_instance']->meta_wp_instance".
+	 */
+	public function get( $request ) {
+		$user_id = ! empty( $request['id'] ) ? (int) $request['id'] : 0;
+
+		if ( empty( $user_id ) ) {
+			return array(
+				'code'    => 'rest_user_no_logged',
+				'message' => __( 'User no logged', 'wp-instance' ),
+				'data'    => array(
+					'status' => 200,
+				),
+			);
+		}
+
+		$instances = get_user_meta( $user_id, \eoxia\Config_Util::$init['wp_instance']->meta_wp_instance, true );
+
+		return $instances;
+	}
+
+	/**
+	 * Supprimes une instance de WordPress pour l'utilisateur current_user_id.
+	 *
+	 * @since 0.1.0
+	 * @version 0.1.0
+	 *
+	 * @param  WP_Rest_Request $request Les données de la requêtes.
+	 * @return array                    Founded WordPress instances without the removed instance.
+	 */
+	public function delete( $request ) {
+		if ( empty( $request['id'] ) || empty( $request['app_id'] ) ) {
+			return false;
+		}
+
+		$user_id = (int) $request['id'];
+
+		$app = array(
+			'id' => (int) $request['app_id'],
+		);
+
+		$meta = get_user_meta( $user_id, \eoxia\Config_Util::$init['wp_instance']->meta_wp_instance, true );
+
+		if ( empty( $meta ) ) {
+			$meta = array();
+		}
+
+		if ( ! empty( $meta ) ) {
+			foreach ( $meta as $key => $m ) {
+				if ( (int) $m['id'] === (int) $app['id'] ) {
+					unset( $meta[ $key ] );
+					break;
+				}
+			}
+		}
+
+		update_user_meta( $user_id, \eoxia\Config_Util::$init['wp_instance']->meta_wp_instance, $meta );
+
+		return $meta;
 	}
 }
 
